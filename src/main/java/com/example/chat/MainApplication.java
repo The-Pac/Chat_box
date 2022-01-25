751572e55183394452bbd941d3a4d57e9ba81050
Pac <debianunix6@gmail.com>
2022-01-25 16:20:34 +0100

    v2 stable et transparent

--- src/main/java/com/example/chat/MainApplication.java
+++ src/main/java/com/example/chat/MainApplication.java
@@ -3,6 +3,7 @@ package com.example.chat;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -18,11 +19,12 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("chat-view-FXML.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 230);
-        stage.setX(0);
+        stage.setX(10);
         stage.setY(0);
         scene.setFill(Color.TRANSPARENT);
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.setTitle("Chat Twitch");
+        stage.getIcons().add(new Image("icon_app.png"));
         stage.setAlwaysOnTop(true);
         stage.setScene(scene);
         stage.show();
